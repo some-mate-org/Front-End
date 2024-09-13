e39a8c4d9e317edec9a4dd9115aee32c550c3404
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-13 17:16:42 +0900

    feat: 정보페이지 생성

---
+++ some-mate/src/pages/PersonalInfoPage/PersonalInfoPage.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+ 
+  margin-bottom: 40px;
+`;
+
+export const Title = styled.h2`
+  color: #6B6969;
+  font-size: 20px;
+  margin-bottom: 20px; 
+  margin-top: 59px;
+
+  
+  span {
+    color: #F76767;
+  }
+`;
