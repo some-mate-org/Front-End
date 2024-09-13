0439626891d4b91657b50e3eb98655ba3ac0ddf2
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-13 17:19:09 +0900

    feat: 버튼 및 라우터 설정 수정

--- some-mate/src/components/Button/index.jsx
+++ some-mate/src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
+// Button.js
 import * as S from './Button.styled';
 
-export default function Button({text, width}) {
-
-  return <S.Button width={width}>{text}</S.Button>;
+export default function Button({ text, width, onClick }) {
+  return <S.Button width={width} onClick={onClick}>{text}</S.Button>;
 }
