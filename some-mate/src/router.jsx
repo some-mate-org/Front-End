794b908a230126c8bb1e0c730fad4913479eef8f
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-20 00:50:57 +0900

    fix: 라우터 경로 수정

--- some-mate/src/router.jsx
+++ some-mate/src/router.jsx
@@ -2,9 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import PageWrapper from './layout/PageWrapper';
 import MainPage from './pages/MainPage';
 import SignPage from './pages/SignPage';
-import SurveyPage from './pages/SurveyPage';
+import SignPage2 from './pages/SignPage2';
+import SignPage3 from './pages/SignPage3';
 import SurveyPage from './pages/SurveyPage';
 
+
+import ResultPage from './pages/ResultPage';
+import MatchingPage from './pages/MatchingPage';
 
 
 export const router = createBrowserRouter([
@@ -19,14 +23,48 @@ export const router = createBrowserRouter([
     children: [{ index: true, element: <SignPage /> }],
   },
   {
-    path: '/survey',
+    path: '/sign2',
     element: <PageWrapper />,
-    children: [{ index: true, element: <SurveyPage /> }],
+    children: [{ index: true, element: <SignPage2 /> }],
+  },
+  {
+    path: '/sign3',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <SignPage3 /> }],
+  },
+  {
+    path: '/result/:result',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <ResultPage /> }],
+  },
+  {
+    path: '/matching',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <MatchingPage /> }],
   },
   {
     path: '/survey',
     element: <PageWrapper />,
     children: [{ index: true, element: <SurveyPage /> }],
   },
+  {
+    path: '/sign2',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <SignPage2 /> }],
+  },
+  {
+    path: '/sign3',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <SignPage3 /> }],
+  },
+  {
+    path: '/result/:result',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <ResultPage /> }],
+  },
+  {
+    path: '/matching',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <MatchingPage /> }],
+  },
 ]);
-
