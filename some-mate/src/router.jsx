529b0ddeabc30c068749f2a06b1768c7565fb6aa
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-19 16:52:53 +0900

    re

--- some-mate/src/router.jsx
+++ some-mate/src/router.jsx
@@ -4,19 +4,11 @@ import MainPage from './pages/MainPage';
 import SignPage from './pages/SignPage';
 import SignPage2 from './pages/SignPage2';
 import SignPage3 from './pages/SignPage3';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import SurveyPage from './pages/SurveyPage';
 
 
-=======
-=======
-import SignPage2 from './pages/SignPage2';
-import SignPage3 from './pages/SignPage3';
->>>>>>> 20401c4 (frontend signup)
 import ResultPage from './pages/ResultPage';
 import MatchingPage from './pages/MatchingPage';
-import SurveyPage from './pages/SurveyPage';
 
 
 export const router = createBrowserRouter([
@@ -54,8 +46,8 @@ export const router = createBrowserRouter([
     path: '/survey',
     element: <PageWrapper />,
     children: [{ index: true, element: <SurveyPage /> }],
-=======
-=======
+  },
+  {
     path: '/sign2',
     element: <PageWrapper />,
     children: [{ index: true, element: <SignPage2 /> }],
@@ -66,7 +58,6 @@ export const router = createBrowserRouter([
     children: [{ index: true, element: <SignPage3 /> }],
   },
   {
->>>>>>> 20401c4 (frontend signup)
     path: '/result/:result',
     element: <PageWrapper />,
     children: [{ index: true, element: <ResultPage /> }],
