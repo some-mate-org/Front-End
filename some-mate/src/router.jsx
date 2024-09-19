cfbba6b3e4fa2703034a7f45abdf6c650a5e8e80
ssohn3 <ssohn3@buffalo.edu>
2024-09-19 15:24:07 +0900

    frontend signup

--- some-mate/src/router.jsx
+++ some-mate/src/router.jsx
@@ -4,6 +4,16 @@ import MainPage from './pages/MainPage';
 import SignPage from './pages/SignPage';
 import SignPage2 from './pages/SignPage2';
 import SignPage3 from './pages/SignPage3';
+<<<<<<< HEAD
+<<<<<<< HEAD
+import SurveyPage from './pages/SurveyPage';
+
+
+=======
+=======
+import SignPage2 from './pages/SignPage2';
+import SignPage3 from './pages/SignPage3';
+>>>>>>> 20401c4 (frontend signup)
 import ResultPage from './pages/ResultPage';
 import MatchingPage from './pages/MatchingPage';
 import SurveyPage from './pages/SurveyPage';
@@ -44,8 +54,19 @@ export const router = createBrowserRouter([
     path: '/survey',
     element: <PageWrapper />,
     children: [{ index: true, element: <SurveyPage /> }],
+=======
+=======
+    path: '/sign2',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <SignPage2 /> }],
   },
   {
+    path: '/sign3',
+    element: <PageWrapper />,
+    children: [{ index: true, element: <SignPage3 /> }],
+  },
+  {
+>>>>>>> 20401c4 (frontend signup)
     path: '/result/:result',
     element: <PageWrapper />,
     children: [{ index: true, element: <ResultPage /> }],
