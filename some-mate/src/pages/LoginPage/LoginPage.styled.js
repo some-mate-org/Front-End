fbc7813bc376deee7f2c2307b526a0fee0e25b1d
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-20 22:13:41 +0900

    feat: 로그인 페이지 추가

---
+++ some-mate/src/pages/LoginPage/LoginPage.styled.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+ margin-left: 27px; 
+`;
+export const InfoText = styled.div`
+  font-size: 15px;
+  color: #737373; 
+  margin-top: 2px; 
+ 
+`;
+
+ export const Text = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #6B6969; 
+  margin-top: 40px; 
+
+`;
+
+export const QuestionText = styled.div`
+  font-size: 16px;
+  font-weight: semi-blod;
+  color: #000000; 
+  margin-top: 100px; 
+  text-align: center;
+
+`;
+
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  margin-top: 20px;    
+  gap: 20px;    
+  margin-right: 25px;
+ 
+`;
