62bd2f827d9d2cc809cd073b67d663e82c0a3438
chanbyeol <cksquf98@naver.com>
2024-09-21 18:25:58 +0900

    resultPage에서 회원가입 결정하도록 수정

--- some-mate/src/pages/SurveyPage/index.jsx
+++ some-mate/src/pages/SurveyPage/index.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import QuCharactor from '../../assets/QuCharactor.svg';
-import * as S from './SurveyPage.styled'; 
+import * as S from './SurveyPage.styled';
 import ProgressBar from '../../components/ProgressBar';
 import SurveyCard from '../../components/SurveyCard';
 import GenderCard from '../../components/GenderCard';
 import { fetchQuestion } from '../../services/surveyService';
-import postUserInfo from '../../services/postUserInfo';
+// import postUserInfo from '../../services/postUserInfo';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../Context/userContext.jsx';
 
@@ -13,31 +13,39 @@ export default function SurveyPage() {
   const [question, setQuestion] = useState({});
   const [answerOptions, setAnswerOptions] = useState([]);
   const [answerList, setAnswerList] = useState([]); // 선택한 값을 담음
-  const [currentQuestionId, setCurrentQuestionId] = useState(1); 
+  const [currentQuestionId, setCurrentQuestionId] = useState(1);
   const [mbti, setMbti] = useState([]); // MBTI 배열 상태 추가
-  const [progress, setProgress] = useState(0); 
+  const [progress, setProgress] = useState(0);
   const { userData, updateUser } = useUser();
-  const TOTAL_QUESTIONS = 10; 
-  const TOTAL_TIME = 60; 
-  const [timeLeft, setTimeLeft] = useState(`완료까지 ${TOTAL_TIME}초 남았어요!`); 
+  const TOTAL_QUESTIONS = 10;
+  const TOTAL_TIME = 60;
+  const [timeLeft, setTimeLeft] = useState(
+    `완료까지 ${TOTAL_TIME}초 남았어요!`
+  );
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    console.log('Updated userData:', userData);
- }, [userData]);
 
   useEffect(() => {
-    console.log(userData)
+    console.log('Updated userData:', userData);
+  }, [userData]);
+
+  useEffect(() => {
+    console.log(userData, currentQuestionId);
     // 질문과 답변 옵션을 가져옴
-    
+
     const loadQuestion = async () => {
       try {
         const questionData = await fetchQuestion(currentQuestionId);
         if (questionData) {
           setQuestion(questionData);
           setAnswerOptions([
-            { text: questionData.answer1_text, value: questionData.answer1_value },
-            { text: questionData.answer2_text, value: questionData.answer2_value }
+            {
+              text: questionData.answer1_text,
+              value: questionData.answer1_value,
+            },
+            {
+              text: questionData.answer2_text,
+              value: questionData.answer2_value,
+            },
           ]);
         }
       } catch (error) {
@@ -45,11 +53,8 @@ export default function SurveyPage() {
       }
     };
 
-    loadQuestion();
+    if (currentQuestionId <= TOTAL_QUESTIONS) loadQuestion();
   }, [currentQuestionId]);
-
-   
-
 
   useEffect(() => {
     // 진행률 업데이트
@@ -57,7 +62,8 @@ export default function SurveyPage() {
     setProgress(newProgress);
 
     // 남은 시간 계산
-    const elapsedTime = ((currentQuestionId - 1) / TOTAL_QUESTIONS) * TOTAL_TIME;
+    const elapsedTime =
+      ((currentQuestionId - 1) / TOTAL_QUESTIONS) * TOTAL_TIME;
     const remainingTime = TOTAL_TIME - elapsedTime;
 
     if (remainingTime <= 5) {
@@ -70,76 +76,79 @@ export default function SurveyPage() {
   const handleAnswerSelect = async (value) => {
     setMbti((prevMbti) => {
       const updatedMbti = [...prevMbti, value];
-  
+
       if (currentQuestionId === TOTAL_QUESTIONS) {
         // 최종 MBTI 계산
         const calculateMBTI = () => {
-          const countOccurrences = (arr, target) => arr.filter(item => item === target).length;
-  
+          const countOccurrences = (arr, target) =>
+            arr.filter((item) => item === target).length;
+
           const E_count = countOccurrences(updatedMbti, 'E');
           const I_count = countOccurrences(updatedMbti, 'I');
           const dominantEI = E_count > I_count ? 'E' : 'I';
-  
+
           const T_count = countOccurrences(updatedMbti, 'T');
           const F_count = countOccurrences(updatedMbti, 'F');
           const dominantTF = T_count > F_count ? 'T' : 'F';
-  
+
           const P_count = countOccurrences(updatedMbti, 'P');
           const J_count = countOccurrences(updatedMbti, 'J');
           const dominantPJ = P_count > J_count ? 'P' : 'J';
-  
+
           const dominantSN = updatedMbti.includes('S') ? 'S' : 'N';
-  
+
           return `${dominantEI}${dominantSN}${dominantTF}${dominantPJ}`;
         };
-  
+
         const mbtiResult = calculateMBTI();
         updateUser({ ...userData, mbti: mbtiResult });
-  
-        const userDataWithMBTI = { ...userData, mbti: mbtiResult };
-  
+        navigate(`/result/${mbtiResult.toLowerCase()}`);
+
+        // const userDataWithMBTI = { ...userData, mbti: mbtiResult };
+
         // async 함수 내에서 await 사용
-        (async () => {
-          try {
-            const response = await postUserInfo(userDataWithMBTI);
-            if (response) {
-              console.log('User registered successfully:', response);
-              navigate(`/result/${mbtiResult.toLowerCase()}`);
-            }
-          } catch (error) {
-            console.error('Error posting user data:', error);
-          }
-        })();
+        // (async () => {
+        //   try {
+        //     const response = await postUserInfo(userDataWithMBTI);
+        //     if (response) {
+        //       console.log('User registered successfully:', response);
+        //       navigate(`/result/${mbtiResult.toLowerCase()}`);
+        //     }
+        //   } catch (error) {
+        //     console.error('Error posting user data:', error);
+        //   }
+
+        // })();
       }
-  
+
       return updatedMbti;
     });
-  
+
     setAnswerList((prevAnswers) => [
       ...prevAnswers,
       { questionId: currentQuestionId, selectedAnswer: value },
     ]);
-  
+
     setCurrentQuestionId((prevId) => prevId + 1);
   };
-  
-  
 
   return (
     <S.PageWrapper>
       <ProgressBar progress={progress} timeLeft={timeLeft} />
-      <S.CustomGenderCardWrapper >
-        <GenderCard imageSrc={QuCharactor} title={question.question1_title}  />
+      <S.CustomGenderCardWrapper>
+        <GenderCard imageSrc={QuCharactor} title={question.question1_title} />
       </S.CustomGenderCardWrapper>
-    
+
       <S.Question>
-        {question.question2_title && typeof question.question2_title === 'string' ? (
+        {question.question2_title &&
+        typeof question.question2_title === 'string' ? (
           <S.QuestionText>{question.question2_title}</S.QuestionText>
         ) : (
           <>
-            <S.QuestionText>{question.question2_title?.questionText || ''}</S.QuestionText>
-            <S.Emoji>{question.question2_title?.emoji || ''}
-            </S.Emoji>
+            <S.QuestionText>
+              {question.question2_title?.questionText || ''}
+            </S.QuestionText>
+            <S.Emoji>{question.question2_title?.emoji || ''}</S.Emoji>
           </>
         )}
       </S.Question>
@@ -154,4 +163,4 @@ export default function SurveyPage() {
       </S.AnswerWrapper>
     </S.PageWrapper>
   );
-};
+}
