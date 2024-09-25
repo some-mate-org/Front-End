db81c2e2a03bc7217fae796b3cc660c11fb3fd00
chanbyeol <cksquf98@naver.com>
2024-09-25 09:43:38 +0900

    SurveyPage 질문리스트 수정사항 반영

--- some-mate/src/services/getQuestionList.js
+++ some-mate/src/services/getQuestionList.js
@@ -4,7 +4,7 @@ const URL = import.meta.env.VITE_API_URL;
 export const getQuestionList = async (setQuestionList) => {
   try {
     const response = await axios.get(`${URL}/questions`);
-    console.log("getQuestionList : ", response.data);
+    console.log('getQuestionList : ', response.data);
     setQuestionList(response.data);
   } catch (error) {
     console.error('Error fetching question list:', error);
