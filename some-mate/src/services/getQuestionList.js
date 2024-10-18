678466747be51b8d6b78a2ea688a92da52af1d49
chanbyeol <cksquf98@naver.com>
2024-10-18 11:15:50 +0900

    add withCredentials to axios request

--- some-mate/src/services/getQuestionList.js
+++ some-mate/src/services/getQuestionList.js
@@ -3,7 +3,12 @@ const URL = import.meta.env.VITE_API_URL;
 
 export const getQuestionList = async (setQuestionList) => {
   try {
-    const response = await axios.get(`${URL}/questions`);
+    const response = await axios({
+      method: 'GET',
+      url: `${URL}/questions`,
+      withCredentials: true, // 이 옵션 추가
+    });
+    console.log('API URL:', URL + '/questions');
     console.log('getQuestionList : ', response.data);
     setQuestionList(response.data);
   } catch (error) {
